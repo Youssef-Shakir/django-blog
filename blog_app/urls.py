12028fa67748a2a48aa997ac99e7ebae1dc80ab6
youssef shakir <youssef.shakir.2003@gmail.com>
2020-11-30 21:20:38 +0300

    laptop

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from blog_app import views
+from .views import PostListView,PostDetailView
 app_name = 'blog_app'
 
 urlpatterns = [
-	path('',views.index,name='home'),
+	path('',PostListView.as_view(),name='home'),
+	path('post/<int:pk>',PostDetailView.as_view(),name='post-detail'),	
 	path('about/',views.about,name='about'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
 from .models import Post
 # Create your views here.
 
 
-def index(request):
-	posts = Post.objects.all()
-	context = {'posts':posts}
-	return render(request,'blog_app/index.html',context)
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog_app/index.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
 
+class PostDetailView(DetailView):
+	model = Post
 
 def about(request):
 	return render(request,'blog_app/about.html')
